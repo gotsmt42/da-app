fff99ff74a20287f24e4d5293de30b89a8276a7b
da-app <baitonggot88@gmail.com>
2024-05-14 09:55:27 +0700

    New update 14/5/67

--- src/layouts/FullLayout.js
+++ src/layouts/FullLayout.js
@@ -1,21 +1,23 @@
+// FullLayout.js
+
 import React, { useRef, useEffect, useState } from "react";
 import { Outlet } from "react-router-dom";
 import Sidebar from "./Sidebar";
 import Header from "./Header";
 import { Container } from "reactstrap";
-import { SpeedInsights } from "@vercel/speed-insights/react";
+import { SpeedInsights } from "@vercel/speed-insights/react"
 import Footer from "./Footer";
-import './FullLayout.css'; // Import CSS file
-
 const FullLayout = () => {
   const sidebarRef = useRef(null);
-  const [isMobile, setIsMobile] = useState(false);
+  const [isMobile, setIsMobile] = useState(false); // State เพื่อติดตามสถานะโหมด mobile
   const [isScrollingUp, setIsScrollingUp] = useState(false);
   const [lastScrollTop, setLastScrollTop] = useState(0);
   const [isHeaderFixed, setIsHeaderFixed] = useState(false);
 
+
   useEffect(() => {
     const handleOutsideClick = (e) => {
+      // ตรวจสอบว่าคลิกอยู่นอกเหนือจาก contentArea
       if (sidebarRef.current && !sidebarRef.current.contains(e.target)) {
         closeSidebar();
       }
@@ -23,12 +25,14 @@ const FullLayout = () => {
 
     const checkIsMobile = () => {
       const screenWidth = window.innerWidth;
-      setIsMobile(screenWidth <= 768);
+      setIsMobile(screenWidth <= 768); // ตั้งค่าให้เป็น mobile เมื่อหน้าจอน้อยกว่าหรือเท่ากับ 768px
     };
-    checkIsMobile();
+    checkIsMobile(); // เรียกฟังก์ชันเมื่อโหลดหน้าเว็บ
 
+    // เพิ่ม event listener เมื่อ component ถูก mount
     document.addEventListener("mousedown", handleOutsideClick);
 
+    // ถอด event listener เมื่อ component ถูก unmount
     return () => {
       document.removeEventListener("mousedown", handleOutsideClick);
     };
@@ -41,50 +45,61 @@ const FullLayout = () => {
   };
 
   const handleMenuClick = () => {
-    closeSidebar();
+    closeSidebar(); // เมื่อคลิกที่เมนูให้ปิด Slidebar
   };
 
   const handleScroll = () => {
     const currentScrollTop = window.pageYOffset || document.documentElement.scrollTop;
-
+  
     setIsScrollingUp(currentScrollTop < lastScrollTop && currentScrollTop > 0);
     setLastScrollTop(currentScrollTop <= 0 ? 0 : currentScrollTop);
-
+  
+    // ตรวจสอบว่าต้องกำหนด Header เป็น fixed หรือไม่
     setIsHeaderFixed(currentScrollTop > 0);
   };
+  
 
   useEffect(() => {
-    window.addEventListener("scroll", handleScroll);
+    window.addEventListener("scroll", handleScroll); // เพิ่ม event listener สำหรับ scroll
 
     return () => {
-      window.removeEventListener("scroll", handleScroll);
+      window.removeEventListener("scroll", handleScroll); // ถอด event listener เมื่อ component ถูก unmount
     };
-  }, [lastScrollTop]);
+  }, [lastScrollTop]); // ให้ useEffect ทำงานเมื่อ lastScrollTop เปลี่ยนแปลง
 
+  // ใช้ตรวจสอบค่า isScrollingUp และใส่คลาส CSS ตามที่ต้องการใน Header
   const headerClass = isScrollingUp ? "stickyHeader" : "";
 
-  return (
-    <main className="d-flex flex-column min-vh-100">
-      <div className={`header ${headerClass}`}>
-        <Header />
-      </div>
-      
-      <div className="pageWrapper d-lg-flex flex-grow-1">
-        <aside className={`sidebarArea shadow ${isMobile ? "" : "showSidebar"}`} id="sidebarArea" ref={sidebarRef}>
-          <Sidebar handleMenuClick={handleMenuClick} />
-        </aside>
+return (
+  <main>
+    {/* Header */}
+    <div className={`header ${headerClass}`}>
+      <Header />
+    </div>
+    
+    <div className="pageWrapper d-lg-flex">
+      {/* Sidebar */}
+      <aside className={`sidebarArea shadow ${isMobile ? "" : "showSidebar"}`} id="sidebarArea" ref={sidebarRef}>
+        <Sidebar handleMenuClick={handleMenuClick} />
+      </aside>
 
-        <div className="contentArea flex-grow-1" onClick={isMobile ? closeSidebar : null}>
-          <Container className="p-4" fluid>
-            <Outlet />
-            <SpeedInsights />
-          </Container>
-        </div>
+      {/* Content Area */}
+      <div className="contentArea" onClick={isMobile ? closeSidebar : null}>
+        {/* Middle Content */}
+        <Container className="p-4" fluid>
+          <Outlet />
+          <SpeedInsights />
+
+        </Container>
+
+        <Footer className="footer" />
+
       </div>
 
-      <Footer className="footer" />
-    </main>
-  );
+
+    </div>
+  </main>
+);
+
 };
-
 export default FullLayout;
